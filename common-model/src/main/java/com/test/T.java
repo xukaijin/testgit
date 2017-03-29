1e1c7bf3c985b119184e4784de5262a49495f03b
kaijin.xu <xu_kaijin@163.com>
2017-03-29 14:31:35 +0800

    test

---
+++ common-model/src/main/java/com/test/T.java
@@ -0,0 +1,33 @@
+package com.test;
+/**
+ * 
+ */
+
+/**
+ * @author xukaijin
+ *
+ */
+public class T {
+
+	private String a;
+	
+	private String b;
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+
+	public String getB() {
+		return b;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+	
+	
+}
